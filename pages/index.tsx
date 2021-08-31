a254eb20db64c38d5c8486ced62111ee3cfa4254
Aashir Aamir Khan <aashir@justaashir.com>
2021-08-31 18:25:27 +0500

    Fix: rainbo

--- pages/index.tsx
+++ pages/index.tsx
@@ -191,7 +191,7 @@ export default function Home() {
                     />
 
                     <div className="text-sm text-gray-800 mt-4">
-                      <RoughNotationGroup show={isFontReady}>
+                      <RoughNotationGroup show={true}>
                         <RainbowHighlight color={colors[3]}>
                           I hate Spam
                         </RainbowHighlight>{" "}
