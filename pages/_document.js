324cf2554bf28f2387c145a1e393515ca10aad7f
Aashir Aamir Khan <aashir@justaashir.com>
2021-08-31 12:53:36 +0500

    Meta Tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+let description = `I lead the front-end team who designed, coded and nurtured the dashboard being used by
+                200,000+ users. currently serving as Head of Product at Kiwify — focused on creating the best experience for digital products seller & buyers.`;
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          {/* META TAGS */}
+          <meta property="og:title" content="Aashir Aamir Khan - Ash" />
+          <meta property="og:description" content={description} />
+          <meta property="og:image" content="/meta-img.png" />
+          <meta property="og:url" content="https://justaashir.com" />
+          <meta name="twitter:card" content="summary_large_image"></meta>
+
           <meta
             name="description"
-            content=" I leaded the front-end team who designed, coded and nurtured the dashboard being used by
-                200,000+ users. currently serving as Head of Product at Kiwify — focused on creating the best experience for digital products seller & buyers."
+            content={description}
           />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
